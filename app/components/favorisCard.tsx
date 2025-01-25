ea5edc71260a369870f6233fdb8fd8599de5c51e
walid lhaila <walidlhaila00@gmail.com>
2025-01-25 20:25:19 +0100

    Feat: CONSOMMATION API TO DELETE A FAVORITE

--- app/components/favorisCard.tsx
+++ app/components/favorisCard.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import { Image } from 'expo-image';
-import {Text, View} from "react-native";
+import {Pressable, Text, View} from "react-native";
 import {Ionicons} from "@expo/vector-icons";
+import {useDispatch, useSelector} from "react-redux";
+import {deleteFavorite, getMyFavorits} from "@/app/redux/slices/favoriteSlice";
+import UseUserData from "@/app/hooks/useUserData";
 
 interface FavorisCardProps {
+    movieId: string;
     title: string;
     time: string;
     description: string;
     img: string;
 }
 
-function FavorisCard({title, time, description, img}: FavorisCardProps) {
+function FavorisCard({title, time, description, img, movieId}: FavorisCardProps) {
     const formattedDate = new Date(time).toISOString().split('T')[0].replace(/-/g, '/');
     const truncatedDescription = description.length > 200 ? description.substring(0, 120) + '...' : description;
     const truncateTitle = title.length > 10 ? title.substring(0, 13) + '...' : title;
+
+    const dispatch = useDispatch();
+    const currentUser = UseUserData();
+    const token = currentUser?.token;
+
+    const handleDelete = async () => {
+        try {
+            await dispatch(deleteFavorite({movieId, token})).unwrap();
+            console.log('FAVORITE DELETED SUCCESSFULLY');
+            await dispatch(getMyFavorits({ token }));
+        } catch (error) {
+            console.log("Error", error || "Failed to delete favorite");
+        }
+    }
     return (
         <View style={{paddingVertical: 10}}>
             <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 10, paddingTop: 0}}>
@@ -24,9 +42,9 @@ function FavorisCard({title, time, description, img}: FavorisCardProps) {
                         <Text style={{color: '#BCC6CC', fontWeight: 'medium', fontFamily: 'serif', fontSize: 12}}>{formattedDate}</Text>
                     </View>
                 </View>
-                <View>
+                <Pressable onPress={handleDelete}>
                     <Ionicons name={"remove-sharp"} color={"white"} size={25} />
-                </View>
+                </Pressable>
             </View>
                 <Text style={{color: '#BCC6CC', paddingHorizontal: 10, paddingTop: 3, fontSize: 12}}>{truncatedDescription}</Text>
         </View>
